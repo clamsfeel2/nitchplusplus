4ef6ab55bf120f6b69cbd5db7eccdb5805a4da2a
Eli <yellowclam@proton.me>
2024-01-23 21:48:14 -0700

    Able to input multiple cli args in single call

--- src/cli-processor.cpp
+++ src/cli-processor.cpp
@@ -13,39 +13,42 @@ int CliProcessor::ProcessCliArgs(int argC, char** argV) {
 	if(args.empty()) {
 		return 1;
 	}
-	std::string flag = args.front();
-	args.pop();
-	if(flag == "-na" || flag == "--noascii") {
-		Configuration::showAscii = false;
-		return 1;
-	} else if(flag == "-h" || flag == "--help") {
-		PrintHelp();
-		return 0;
-	} else if(flag == "-w" || flag == "--width") {
-		if(args.empty()) {
-			oss << C::B_RED << "ERROR: " << C::NC << "invalid number of arguments"<< std::endl << C::B_WHITE << "Run 'fetchpp --help' to see a list of legal commands.";
-			throw std::invalid_argument(oss.str());
-		} else {
+	while(!args.empty()) { 
+		std::string flag = args.front();
+		args.pop();
+		if(flag == "-na" || flag == "--noascii") {
+			Configuration::showAscii = false;
+		} else if(flag == "-h" || flag == "--help") {
+			PrintHelp();
+			return 0;
+		} else if(flag == "-w" || flag == "--width") {
+			if(args.empty()) {
+				oss << C::B_RED << "ERROR: " << C::NC << "invalid number of arguments"<< std::endl << C::B_WHITE << "Run 'fetchpp --help' to see a list of legal commands.";
+				throw std::invalid_argument(oss.str());
+			} else {
+				Configuration::width = std::stoi(args.front());
+			}
+			if(std::stoi(args.front()) < 5) {
+				oss << C::B_RED << "ERROR: " << C::NC << "width value in your config file MUST be greater than 5 or else my display breaks :(";
+				throw std::invalid_argument(oss.str());
+			}
 			Configuration::width = std::stoi(args.front());
-		}
-		if(std::stoi(args.front()) < 5) {
-			oss << C::B_RED << "ERROR: " << C::NC << "width value in your config file MUST be greater than 5 or else my display breaks :(";
-			throw std::invalid_argument(oss.str());
-		}
-		Configuration::width = std::stoi(args.front());
-		Configuration::widthSupplied = true;
-	} else if(flag == "-d" || flag == "--distro") {
-		if(args.empty()) {
-			oss << C::B_RED << "ERROR: " << C::NC << "invalid number of arguments"<< std::endl << C::B_WHITE << "Run 'fetchpp --help' to see a list of legal commands.";
-			throw std::invalid_argument(oss.str());
-		} else {
-			Configuration::distroSuppliedFromCli = true;
-			Configuration::tmpDistro = args.front();
-		}
+			Configuration::widthSupplied = true;
+			args.pop();
+		} else if(flag == "-d" || flag == "--distro") {
+			if(args.empty()) {
+				oss << C::B_RED << "ERROR: " << C::NC << "invalid number of arguments"<< std::endl << C::B_WHITE << "Run 'fetchpp --help' to see a list of legal commands.";
+				throw std::invalid_argument(oss.str());
+			} else {
+				Configuration::distroSuppliedFromCli = true;
+				Configuration::tmpDistro = args.front();
+				args.pop();
+			}
 
-	} else {
-		oss << C::B_RED << "ERROR: " << C::NC << "incorrect usage \"" << flag << "\"" << std::endl << C::B_WHITE << "Run 'fetchpp --help' to see a list of legal commands.";
-		throw std::invalid_argument(oss.str());
+		} else {
+			oss << C::B_RED << "ERROR: " << C::NC << "incorrect usage \"" << flag << "\"" << std::endl << C::B_WHITE << "Run 'fetchpp --help' to see a list of legal commands.";
+			throw std::invalid_argument(oss.str());
+		}
 	}
 	return 1;
 }
